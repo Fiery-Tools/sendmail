3dbcee6d3f21578da2841509de253f9f93b9c44c
P Guardiario <p.guardiario@gmail.com>
2025-10-15 10:39:18 +0800

    readme

--- send-test-email.js
+++ send-test-email.js
@@ -1,7 +1,7 @@
 import mail from './index.js';
 
 const to = 'pguardiario@gmail.com';
-const from = 'support@mail.fiery.tools';
+const from = 'elon@x.com';
 const subject = 'Test Email from Fiery Tools';
 const html = '<h1>Hello World!</h1><p>This is a test email sent from a Node.js application on a Contabo VPS.</p>';
 
