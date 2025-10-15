d84364c9b979a2c504037309567ddcefdfda1e02
P Guardiario <p.guardiario@gmail.com>
2025-10-15 09:56:06 +0800

    init

--- send-test-email.js
+++ send-test-email.js
@@ -1,8 +1,9 @@
 import mail from './index.js';
 
 const to = 'pguardiario@gmail.com';
-const from = 'support@fiery.tools';
+const from = 'support@mail.fiery.tools';
 const subject = 'Test Email from Fiery Tools';
 const html = '<h1>Hello World!</h1><p>This is a test email sent from a Node.js application on a Contabo VPS.</p>';
 
 mail(to, from, subject, html);
+"v=spf1 include:_spf.mx.cloudflare.net ip4:173.249.24.21 ~all"
